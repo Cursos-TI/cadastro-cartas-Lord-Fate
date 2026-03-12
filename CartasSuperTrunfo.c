e8ec47395eeb7a0c0fc35c2855d9cd558611dc38
Lord-Fate <267469404+Lord-Fate@users.noreply.github.com>
2026-03-12 00:19:52 +0000

    Desafio Nivel Novato: Começei a adicionar as linhas e comentários de explicação do que fiz na área de desenvolvimen to do meu codigo para o jogo Supertrunfo.
    Definição de Variáveis
    
    char: estado 1 e 2 , codigo 1 e 2, cidade 1 e 2.
    int: população 1 e 2, ponto turistico 1 e 2
    float: pib 1 e 2.
    
    Entrada de dados
    printf: cadastro de carta 1, digite a letra do
    estado, digite codigo da carta.
    scanf: %c &estado 1 e %s codigo1.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,8 +7,23 @@
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
 
+  char estado1, estado 2; // armazena a letra do estado das cartas (1 e 2)
+  char codigo1[4], codigo2[4]; // armazena o código da carta (ex:A01, B02)
+  char cidade1[50], cidade2[50]; // armazena o nome da cidade
+  int população1, população2; // armazena a população da cidade
+  float pib1, pib2; // armazena o PIB da cidade
+  int pontosTuristicos1, pontosTuristicos2; // armazena o número de pontos turísticos
+  
   // Área para entrada de dados
 
+  printf("Cadastro da Carta 1\n"); //
+  
+  printf("Digite a letra do estado (A-H): "); // solicita ao usuário a letra do estado
+  scanf("%c, &estado1"); // lê a letra do estado digitada pelo usuário
+  
+  printf("Digite o codigo da carta (ex:A01): "); // solicita o código da carta
+  scanf("%s", codigo1); // lê o código da carta
+  
   // Área para exibição dos dados da cidade
 
 return 0;
